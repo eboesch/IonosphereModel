e8062f9eb64208bd6e1cf38dca0c90926dfdbdd5
martin-rodriguez-munoz <68546107+martin-rodriguez-munoz@users.noreply.github.com>
2025-04-01 15:18:24 +0200

    Update main.py
    fix mae loss

--- main.py
+++ main.py
@@ -206,7 +206,7 @@ if __name__ == "__main__":
 
 
     logger.info("Starting evaluation...")
-    eval_loss_fct = nn.MAELoss()
+    eval_loss_fct = nn.L1Loss()
     test_loss = test(dataloader_test, model, eval_loss_fct, device)
     logger.info(f"Evaluation MAE Loss: {test_loss:>7f}")
     logger.info("Completed.")
