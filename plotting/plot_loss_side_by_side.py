c8af0158a8e42e06f33222493ac03571f9218772
Elena Boesch <eboesch@eu-login-18.euler.ethz.ch>
2025-05-29 21:26:16 +0200

    removed old, unused model paths

--- plotting/plot_loss_side_by_side.py
+++ plotting/plot_loss_side_by_side.py
@@ -13,18 +13,10 @@ dslab_path = "/cluster/work/igp_psr/dslab_FS25_data_and_weights/"
 # pretrained models
 
 pretrained_model_dict = { # dictionary of pretrained model locations
-    "model_2025-04-25-19-09-00/": "Hector 1", 
-    "model_2025-04-25-15-03-51/": "Hector 2", 
-    "model_2025-04-28-09-08-52/": "Hector 3", 
-    "model_2025-04-30-14-05-28/": "4+1",
-    "model_2025-04-30-21-02-28/": "6 old", 
-    "model_2025-04-30-23-22-21/": "4+2", 
-    "model_2025-05-19-17-11-14/": "6", 
-    # "model_2025-04-30-23-22-21/": "4+2", 
-    "model_2025-05-01-16-14-32/": "6+3", 
-    "model_2025-05-01-23-02-56/": "4+2, with smaller batch size",
-    "model_2025-05-04-12-10-18/": "4+2, with MAE training loss",    
-    "model_2025-05-10-21-34-55/": "hourly solar indices, 4+2, MAE",
+    "model_2025-05-19-17-11-14/": "6 MSE", 
+    "model_2025-04-30-23-22-21/": "4+2 MSE", 
+    "model_2025-05-04-12-10-18/": "4+2 MAE",    
+    "model_2025-05-19-15-24-01/": "hourly solar indices, 4+2, MAE",
     "model_2025-05-10-17-39-31/": "daily solar indices, 4+2, MAE",    
 }
 
@@ -38,7 +30,7 @@ if MODEL_TYPE == "pretrained":
     outpath = 'outputs/val_loss_pt_double.png'
 
     # select which models you want to plot
-    model_labels1 = ["6", "4+2"]
+    model_labels1 = ["6 MSE", "4+2 MSE"]
     model_labels2 = ["hourly solar indices, 4+2, MAE", "daily solar indices, 4+2, MAE"]
 
     models1 = [pretrained_model_dict[model] for model in model_labels1]
@@ -49,39 +41,14 @@ if MODEL_TYPE == "pretrained":
 # fine tuned models
 
 finetuned_model_dict = { # dictionary of finetuned model locations
-    "model_2025-04-28-12-21-37/": "Hector 1",
-    "model_2025-04-28-12-37-02/": "Hector 2",
-    "model_2025-04-28-12-50-35/": "Hector 3",
-    "model_2025-04-28-13-02-05/": "Hector 4",
-    "model_2025-04-28-13-13-17/": "Hector 5",
-    "model_2025-05-01-13-51-30/": "4+1",
-    "model_2025-05-01-14-03-22/": "6",
-    "model_2025-05-01-14-35-02/": "4+2",
-    "model_2025-05-01-14-48-57/": "4+2 0.5 FT batch size", 
-    "model_2025-05-01-15-09-01/": "4+2 0.5 FT LR", 
-    "model_2025-05-01-15-55-55/": "from scratch, 4", 
-    "model_2025-05-02-09-41-24/": "6+3", 
-    "model_2025-05-02-09-50-42/": "4+2 0.5 batch size", 
-    "model_2025-05-02-10-03-18/": "6+3 0.5 FT LR", 
-    "model_2025-05-02-10-23-23/": "4+2 0.1 FT LR", 
-    "model_2025-05-02-10-36-41/": "4+2, day 7", 
-    "model_2025-05-02-10-52-15/": "4+2, day 14", 
-    "model_2025-05-02-11-14-16/": "4+2, day 28",
-    "model_2025-05-04-21-40-49/": "4+2, MAE bad formatting",
-    "model_2025-05-05-11-15-10/": "4+2, MAE 0.5 FT LR",
-    "model_2025-05-05-11-31-39/": "4+2, MAE",
-    "model_2025-05-05-13-23-28/": "from scratch, 4, MAE, with doy+year", 
-    "model_2025-05-05-13-37-53/": "from scratch, 4, MAE", 
-    "model_2025-05-11-15-32-59/": "hourly solar indices, 4+2, MAE",
-    "model_2025-05-11-16-53-38/": "daily solar indices, 4+2, MAE",
-    "model_2025-05-19-15-22-50/": "4+2 MSE",
-    "model_2025-05-20-09-58-46/": "6 MSE",
-    "model_2025-05-19-15-51-39/": "6 MSE bad run",
     "model_2025-05-19-16-24-04/": "From scratch MSE",
-    "model_2025-05-19-16-44-58/": "From scratch MSE 7",
+    "model_2025-05-20-09-58-46/": "6 MSE",
+    "model_2025-05-19-15-22-50/": "4+2 MSE",
+    "model_2025-05-04-21-40-49/": "4+2 MAE",
+    "model_2025-05-11-16-53-38/": "daily solar indices, 4+2, MAE", # same as "Day 1, finetuned"
+    "model_2025-05-20-10-29-44/": "hourly solar indices, 4+2, MAE",
     "model_2025-05-20-11-23-28/": "From scratch with daily solar indices",
-    "model_2025-05-11-16-53-38/": "Day 1, finetuned",
-    "model_2025-05-20-11-23-28/": "Day 1, from scratch",
+    "model_2025-05-20-11-37-14/": "From scratch with hourly solar indices", # same as "Day 1, from scratch"
     "model_2025-05-21-11-00-30/": "Day 8, finetuned",
     "model_2025-05-21-12-39-41/": "Day 8, from scratch",
     "model_2025-05-21-10-45-05/": "Day 16, finetuned",
@@ -91,7 +58,6 @@ finetuned_model_dict = { # dictionary of finetuned model locations
     "model_2025-05-21-10-46-14/": "Day 64, finetuned",
     "model_2025-05-21-11-41-03/": "Day 64, from scratch",
 }
-
 # flip dictionary
 finetuned_model_dict = {value: key for key, value in finetuned_model_dict.items()}
 
